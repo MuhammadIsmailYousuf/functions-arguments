76dbbcd63809dfd413b8cd32aeea8ccf9794b9cd
MuhammadIsmailYousuf <ismailyousuf192005@gmail.com>
2024-07-11 23:26:12 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+//Task 1 
+// function with a parameter called 'name'
+function greet(name) {
+    console.log(`Hello ${name}`);
+}
+
+// pass argument to the function
+greet("world!");
+
+// Output => "Hello world"
+
+//Task 2
+function parameter(name) {
+    console.log(`Hello ${name}`);
+}
+
+// pass "John" as argument
+greet("John");
+
+// pass "David" as argument
+greet("sir!");
+
+// output => "Hello John" , "Hello sir!"
+
+
+//Task 3
+function read(data){
+       console.log(`20 + 40 =  ${data}`);
+}
+
+function read1(data1){
+    console.log(`455 - 5 = ${data1}`);
+}
+//pass "60" as argumemt
+read("60")
+
+//pass numbers "455" as argument
+read1("450")
